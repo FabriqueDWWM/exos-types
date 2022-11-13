d68782c6f5c51b593622f9a684ffca080980f8e6
webew <fillion.bertrand@gmail.com>
2022-11-13 17:36:42 +0100

    ajout exo

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// types primitifs
+let myNumber = 12;
+let myString = "Hye !";
+let myBoolean = true;
+let myNull = null;
+let myUndefined = undefined;
+
+// combinaisons de types
+let nan = myNumber * myString;
+let myConcat = myString + myNumber;
+
+// tableaux et objets
+let myArray = [12, 23, 45, 67, 89];
+let myObjet = { id: 1, name: "Toto" };
+
+// Eléments du DOM
+let myBody = document.getElementsByTagName("body");
+let myUl = document.getElementsByTagName("ul");
